bd614b9f00e81c0397cd0bfce741b0ef9f9b7421
kanishkanagrare <68229126+kanishkanagrare@users.noreply.github.com>
2020-08-25 20:31:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+
+function preload(){
+
+}
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
